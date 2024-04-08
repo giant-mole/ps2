0c0735f217d8c131739eab7e96eaa1c04462a9b7
giant-mole <75571209+giant-mole@users.noreply.github.com>
2024-04-08 21:58:20 +0200

    Add files via upload
    feedback van mellanie doorgevoerd:
    je code netter maken, staan een aantal lege regels tussen
    je hebt twee divs die je opent in de nav maar niet sluit
    bij privacy en nieuws, lidmaatschap, aanvullende informatie ontbreekt de </body> tag en </html> 
    mischien de js code in een apparte file zetten om het netter te maken

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
 
 // testing the webpage scripts
 console.log('Hello World!');
-
+// is van w3 schools
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
